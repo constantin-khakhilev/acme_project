165ba70d492a30b25dd32c3dd23ad1e911aabc59
Konstantin Khakhilev <constantin.khakhilev@gmail.com>
2023-10-18 22:24:42 +0700

    Спринт 7/19: 7 спринт → Тема 2/3: Работа с формами → Урок 9/10

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,13 @@
 from django import forms
-
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
 from .models import Birthday
+
+# Импортируем функцию-валидатор.
+from .validators import real_age
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
 class BirthdayForm(forms.ModelForm):
@@ -11,3 +18,38 @@ class BirthdayForm(forms.ModelForm):
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
+
+
+"""
+class BirthdayForm(forms.Form):
+    first_name = forms.CharField(label='Имя', max_length=20)
+    last_name = forms.CharField(
+        label='Фамилия', required=False, help_text='Необязательное поле'
+    )
+    birthday = forms.DateField(
+        label='Дата рождения',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        # В аргументе validators указываем список или кортеж 
+        # валидаторов этого поля (валидаторов может быть несколько).
+        validators=(real_age,),
+    )
+"""
